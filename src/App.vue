945189b26b873185ebd5ababcb9617d7b9f3146c
IrisChau02 <117070549+IrisChau02@users.noreply.github.com>
2023-04-23 17:45:11 +0800

    n

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,5 @@
   <!--this template will be shown at all time-->
   <template>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">Home</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-      aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-  </nav>
   <router-view />
 </template>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,26 +1,27 @@
 <template>
   <div>
     <div class="fullscreen-background" :style="{ 'backgroundImage': 'url(bg.png)' }">
-      <div class="container-fluid h-75 d-flex align-items-center justify-content-center">
 
-        <div class="card text-center" style="width: 30rem">
-          <div class="card-header" style="font-size: 32px;">
-            余月宴籌號進度
-          </div>
-          <div class="card-body" style="font-size: 24px;">
-            <p>現時籌後正派到 xxx - xxx</p>
-            <p>現在可排隊進場籌號為：xxxxxx</p>
-            <p>請各位耐心等候，謝謝。 </p>
+        <div class="container-fluid h-75 d-flex align-items-center justify-content-center">
+          <div class="card text-center" style="width: 30rem">
+            <div class="card-header" style="font-size: 32px;">
+              余月宴籌號進度
+            </div>
+            <div class="card-body" style="font-size: 24px;">
+              <p>現時籌後正派到 xxx - xxx</p>
+              <p>現在可排隊進場籌號為：xxxxxx</p>
+              <p>請各位耐心等候，謝謝。 </p>
+            </div>
           </div>
         </div>
-      </div>
+  
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "LoginView",
+  name: "HomeView",
   components: {
 
   },
@@ -36,7 +37,6 @@ hr {
   color: #191270;
   font-weight: bold;
 }
-
 
 .fullscreen-background {
   position: fixed;
